76989d41dc25a61d776888d787e3e55f4580a70b
Mstriker64 <mitchellthanath@gmail.com>
2024-09-14 14:40:59 -0400

    TechNews Project update

--- js/scripts.js
+++ js/scripts.js
@@ -185,3 +185,33 @@ function showSlides5(n) {
   slides[slideIndex5-1].style.display = "block";
   dots[slideIndex5-1].className += " active";
 }
+
+// Project 7
+let slideIndex6 = 1;
+showSlides6(slideIndex6);
+
+// Next/previous controls
+function plusSlides6(n) {
+  showSlides6(slideIndex6 += n);
+}
+
+// Thumbnail image controls
+function currentSlide6(n) {
+  showSlides6(slideIndex6 = n);
+}
+
+function showSlides6(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides6");
+  let dots = document.getElementsByClassName("dot6");
+  if (n > slides.length) {slideIndex6 = 1}
+  if (n < 1) {slideIndex6 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex6-1].style.display = "block";
+  dots[slideIndex6-1].className += " active";
+}
